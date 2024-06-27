64f0fb7e4e07d6938fbf17b78a89734fffc6e8fe
Hamza Bin Mazhar <hmazhar@pulsarmeasurement.com>
2024-06-27 09:31:49 +0100

    Test 28 for bluefy using self console log

--- sw.js
+++ sw.js
@@ -1,33 +1,33 @@
-const CACHE_NAME = 'xml-cache-v1';
+const CACHE_NAME = "xml-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './sample.xml' // Path to your XML file
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./sample.xml", // Path to your XML file
 ];
 
 // Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  self.console.log("Installed!");
 });
 
 // Fetch event
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   const requestUrl = new URL(event.request.url);
 
-  if (requestUrl.pathname.endsWith('./sample.xml')) {
+  if (requestUrl.pathname.endsWith("./sample.xml")) {
     event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(event.request).then(response => {
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
           if (response) {
+            self.console.log("From cache!");
             return response;
           } else {
-            return fetch(event.request).then(networkResponse => {
+            return fetch(event.request).then((networkResponse) => {
               cache.put(event.request, networkResponse.clone());
+              self.console.log("Network response!");
               return networkResponse;
             });
           }
@@ -36,7 +36,8 @@ self.addEventListener('fetch', event => {
     );
   } else {
     event.respondWith(
-      caches.match(event.request).then(response => {
+      caches.match(event.request).then((response) => {
+        self.console.log("Path URL not match!");
         return response || fetch(event.request);
       })
     );
